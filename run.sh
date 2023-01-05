9fdb49a4084af2a5f83b672b8936f9870e2f1826
Roberto Santalla <roobre@roobre.es>
2023-01-05 17:27:39 +0100

    install: fix parallel download patch

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ for file in user_configuration.json user_disk_layout.json; do
     curl http://10.4:3000/archinstall/$file > $file
 done 
 
-echo "ParallelDownloads = 8" >> /etc/pacman.conf
+sed -i "s/^#ParallelDownloads.*/ParallelDownloads = 8/" /etc/pacman.conf
 
 archinstall --config user_configuration.json --disk-layout user_disk_layout.json
