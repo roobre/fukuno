a0fefae09dcf2ae54f58bfbd48944fb54cfcf999
Roberto Santalla <roberto@santalla.io>
2023-05-14 22:31:41 +0200

    prepare comments for using mitogen

--- play.sh
+++ play.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Mitogen 0.3.3 is not compatible with ansible 2.16.x
+# export ANSIBLE_STRATEGY_PLUGINS="$(pip show mitogen | grep -E '^Location: ' | sed -e 's/^Location: \(.*\)/\1/')/ansible_mitogen/plugins/strategy/"
+
 playbook=$1
 shift
 
